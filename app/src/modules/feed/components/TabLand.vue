d93a17f7677ce3c63ecda4ee54c64de5171b00a0
Alberto Ramirez <57710147+NeoMako@users.noreply.github.com>
2021-10-04 23:22:37 +0200

    inventario

--- app/src/modules/feed/components/TabLand.vue
+++ app/src/modules/feed/components/TabLand.vue
@@ -64,55 +64,5 @@ export default {
 @import "@/css/colors.scss";
 @import "@/css/animations.scss";
 
-.inventory-view-land {
-  width: 100%;
-  height: 150px;
-}
-.inventory-view-land .coordinate.capacity {
-  filter: none;
-  background-color: transparent;
-  border: none;
-}
-.inventory-view-land .time {
-  border: 2px solid rgb(0 0 0 / 15%);
-  background-color: rgb(0 0 0 / 50%);
-  font-size: x-small;
-  color: #fff;
-}
-.inventory-view-land .coordinate.have-house {
-  filter: none;
-  background-color: transparent;
-  border: none;
-  margin-left: 67px;
-}
-.inventory-view-land .coordinate.have-light {
-  margin-left: 87px;
-  background-color: #f4c300;
-  border: none;
-  padding: 2px 10px;
-}
-.inventory-view-land .floor {
-  width: 161px;
-  height: 100px;
-  margin-top: 20px;
-}
-.inventory-view-land .time.quantity {
-  filter: none;
-  background-color: transparent;
-  border: none;
-}
-.inventory-view-land .pet {
-  height: 50%;
-  animation: none;
-  margin-top: -10px;
-}
 
-.disabled,
-.disabled:hover,
-.disabled:focus,
-.disabled:active {
-  cursor: no-drop !important;
-  background-color: gray !important;
-  box-shadow: none !important;
-}
 </style>
